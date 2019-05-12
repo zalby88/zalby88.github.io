4fbbb1fb4f6473f938eef1f9f164f0bcbd81c34d
zalby88 <zalby88@hotmail.it>
2019-05-12 11:47:42 +0100

    - Saved old version as ZIP - added IIFE patterns - some clean up

--- script/myCvScript.js
+++ script/myCvScript.js
@@ -3,53 +3,73 @@ if (AlbertoLazzarinCV == null) {
 	AlbertoLazzarinCV = {};
 }
 
+// ************************
+// UTILITY FUNCTIONS SETUP
+// ************************
+
 if (AlbertoLazzarinCV.myFunctions == null) {
-
-	AlbertoLazzarinCV.myFunctions = {
 	
-		showHideAccordion: function (componentId) {
-			var isHidden = document.getElementById(componentId).getAttribute('hidden');
-			
-			if (isHidden != null) {
-				var isNowHidden = true;
-				if (isHidden == "true") {
-					isNowHidden = false;
-				}
-				document.getElementById(componentId).setAttribute('hidden', isNowHidden);
-			}
-		} , 
+	//IIFE pattern
+	AlbertoLazzarinCV.myFunctions = (function () {
 		
-		selectComkSkillsButton: function (selectedComponent) {
-			selectedComponent.className = 'button selected';
-			var sectionToOpen = selectedComponent.getAttribute("sectionToOpen");
-			
-			//Open the corresponding section and close the others
-			document.getElementById("programmingSkillsPanel").setAttribute('hidden', true);
-			document.getElementById("webDesignSkillsPanel").setAttribute('hidden', true);
-			document.getElementById("dbmsSkillsPanel").setAttribute('hidden', true);
-			if (sectionToOpen == 'programmingSkills') {
-				
-				document.getElementById("programmingSkillsPanel").setAttribute('hidden', false);
-				
-			} else if (sectionToOpen == 'webDesignSkills') {
-				
-				document.getElementById("webDesignSkillsPanel").setAttribute('hidden', false);
-				
-			} else if (sectionToOpen == 'dbmsSkills') {
-				
-				document.getElementById("dbmsSkillsPanel").setAttribute('hidden', false);
-			}
-			
-			//Deselect the other buttons
-			var elemParent = selectedComponent.parentNode.parentNode.parentNode; //I want to reach "table" element
-			var childrenButtons = elemParent.getElementsByClassName('button');
-			for (var j = 0; j < childrenButtons.length; j++) {
-				if (childrenButtons[j] != selectedComponent) {
-					childrenButtons[j].className = 'button';
+		//**************
+		//Private Fields
+		//**************
+		let _compSkillsPanelSuffix = "_panel";
+		
+		let _showElelementById = function(elementId) { 
+			document.getElementById(elementId).setAttribute('hidden', false);
+		};
+		
+		let _hideElelementById = function(elementId) {
+			document.getElementById(elementId).setAttribute('hidden', true);
+		};
+		
+		
+		//Computer Skills private functions
+		let _hideComSkillsPanels = function() {
+			document.getElementById("programmingSkills" + _compSkillsPanelSuffix).setAttribute('hidden', true);
+			document.getElementById("webDesignSkills" + _compSkillsPanelSuffix).setAttribute('hidden', true);
+			document.getElementById("dbmsSkills" + _compSkillsPanelSuffix).setAttribute('hidden', true);
+		};
+		
+		//**************
+		//Exposed Fields
+		//**************
+		return {
+			showHideAccordion: function (componentId) {
+				let isHidden = document.getElementById(componentId).getAttribute('hidden');
+				if (isHidden === "true") {
+					_showElelementById(componentId);
+				} else {
+					_hideElelementById(componentId);
 				}
+			} , 
+			
+			selectCompSkillsButton: function (selectedComponent) {
 				
+				//Close all the sections
+				_hideComSkillsPanels();
+				
+				//Open the corresponding section
+				let comSkillPanelToOpen = 
+					selectedComponent.getAttribute("data-zalbycv-section-to-open") + _compSkillsPanelSuffix;
+		
+				_showElelementById(comSkillPanelToOpen);
+				
+				//Select only the button clicked
+				let elemParent = document.getElementById("computerSkillsBox");
+				let childrenButtons = elemParent.getElementsByClassName('button');
+				for (let j = 0; j < childrenButtons.length; j++) {
+					if (childrenButtons[j] != selectedComponent) {
+						childrenButtons[j].className = 'button';
+					} else {
+						childrenButtons[j].className = 'button selected';
+					}
+					
+				}
 			}
-		}
-
-	};
+		}//endof returning public objects
+		
+	})();//endof defining function
 }
